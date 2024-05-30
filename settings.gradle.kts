abf71670c88ce657c3e533a17bffa1ee68a08c52
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-05-30 15:31:35 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.17.4"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.6"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks()
+}
+
+rootProject.name = "Template-for-Kotlin-Multiplatform-Projects".lowercase()
