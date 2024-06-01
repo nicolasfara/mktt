2932892d0cccab51bd7de9c3d9cc4884c0b5d508
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-01 12:38:58 +0200

    test: add client test base class

---
+++ src/commonTest/kotlin/it/nicolasfarabegoli/mktt/MqttClientTest.kt
@@ -0,0 +1,9 @@
+package it.nicolasfarabegoli.mktt
+
+import io.kotest.core.spec.style.FreeSpec
+
+class MqttClientTest : FreeSpec({
+    "The client should connect to the broker with default parameters" {
+        // TODO
+    }
+})
