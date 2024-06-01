28310ce2c9b9134b13ba6a58d6d43faf422ce666
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-01 17:48:46 +0200

    refactor: move into generic message type

--- src/commonMain/kotlin/it/nicolasfarabegoli/mktt/message/MqttMessage.kt
+++ src/commonMain/kotlin/it/nicolasfarabegoli/mktt/message/MqttMessage.kt
@@ -1,25 +1,10 @@
 package it.nicolasfarabegoli.mktt.message
 
-data class MqttMessage(val topic: String, val payload: ByteArray, val qos: QoS, val retain: Boolean = false) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
+import it.nicolasfarabegoli.mktt.topic.MqttTopic
 
-        other as MqttMessage
-
-        if (topic != other.topic) return false
-        if (!payload.contentEquals(other.payload)) return false
-        if (qos != other.qos) return false
-        if (retain != other.retain) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = topic.hashCode()
-        result = 31 * result + payload.contentHashCode()
-        result = 31 * result + qos.hashCode()
-        result = 31 * result + retain.hashCode()
-        return result
-    }
-}
+data class MqttMessage<MessageType>(
+    val message: MessageType,
+    val topic: MqttTopic,
+    val qos: QoS = ExactlyOnce,
+    val retain: Boolean = false,
+)
