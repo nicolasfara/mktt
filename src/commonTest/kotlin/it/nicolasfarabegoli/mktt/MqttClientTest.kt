3842177c10c1050c9a4c93fd581de2587d4d7508
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-03 14:49:34 +0200

    refactor: add conn ack as return of conncet method

--- src/commonTest/kotlin/it/nicolasfarabegoli/mktt/MqttClientTest.kt
+++ src/commonTest/kotlin/it/nicolasfarabegoli/mktt/MqttClientTest.kt
@@ -3,7 +3,9 @@ package it.nicolasfarabegoli.mktt
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.test.testCoroutineScheduler
+import io.kotest.matchers.shouldBe
 import it.nicolasfarabegoli.mktt.configuration.MqttConfiguration
+import it.nicolasfarabegoli.mktt.message.connect.connack.MqttConnAckReasonCode
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 
@@ -14,7 +16,7 @@ class MqttClientTest : FreeSpec({
         val dispatcher = StandardTestDispatcher(testCoroutineScheduler)
         val mqttClient = MqttClient(MqttConfiguration(hostname = "test.mosquitto.org"), dispatcher)
         shouldNotThrow<Exception> {
-            mqttClient.connect()
+            mqttClient.connect() shouldBe MqttConnAckReasonCode.Success
             mqttClient.disconnect()
         }
     }
