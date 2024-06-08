f9cc4f4aec97a2b061bde5a3801205c40346a59f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-08 15:36:54 +0200

    refactor: move inside interface data object

--- src/commonMain/kotlin/it/nicolasfarabegoli/mktt/message/MqttMessageType.kt
+++ src/commonMain/kotlin/it/nicolasfarabegoli/mktt/message/MqttMessageType.kt
@@ -2,64 +2,84 @@ package it.nicolasfarabegoli.mktt.message
 
 sealed interface MqttMessageType {
     val code: Byte
-}
 
-data object Connect : MqttMessageType {
-    override val code: Byte = 0x01.toByte()
-}
+    companion object {
+        fun values(): List<MqttMessageType> = listOf(
+            Connect,
+            ConnAck,
+            Publish,
+            PubAck,
+            PubRec,
+            PubRel,
+            PubComp,
+            Subscribe,
+            SubAck,
+            Unsubscribe,
+            UnsubAck,
+            PingReq,
+            PingResp,
+            Disconnect,
+            Auth,
+        )
+    }
 
-data object ConnAck : MqttMessageType {
-    override val code: Byte = 0x02.toByte()
-}
+    data object Connect : MqttMessageType {
+        override val code: Byte = 0x01.toByte()
+    }
 
-data object Publish : MqttMessageType {
-    override val code: Byte = 0x03.toByte()
-}
+    data object ConnAck : MqttMessageType {
+        override val code: Byte = 0x02.toByte()
+    }
 
-data object PubAck : MqttMessageType {
-    override val code: Byte = 0x04.toByte()
-}
+    data object Publish : MqttMessageType {
+        override val code: Byte = 0x03.toByte()
+    }
 
-data object PubRec : MqttMessageType {
-    override val code: Byte = 0x05.toByte()
-}
+    data object PubAck : MqttMessageType {
+        override val code: Byte = 0x04.toByte()
+    }
 
-data object PubRel : MqttMessageType {
-    override val code: Byte = 0x06.toByte()
-}
+    data object PubRec : MqttMessageType {
+        override val code: Byte = 0x05.toByte()
+    }
 
-data object PubComp : MqttMessageType {
-    override val code: Byte = 0x07.toByte()
-}
+    data object PubRel : MqttMessageType {
+        override val code: Byte = 0x06.toByte()
+    }
 
-data object Subscribe : MqttMessageType {
-    override val code: Byte = 0x08.toByte()
-}
+    data object PubComp : MqttMessageType {
+        override val code: Byte = 0x07.toByte()
+    }
 
-data object SubAck : MqttMessageType {
-    override val code: Byte = 0x09.toByte()
-}
+    data object Subscribe : MqttMessageType {
+        override val code: Byte = 0x08.toByte()
+    }
 
-data object Unsubscribe : MqttMessageType {
-    override val code: Byte = 0x0A.toByte()
-}
+    data object SubAck : MqttMessageType {
+        override val code: Byte = 0x09.toByte()
+    }
 
-data object UnsubAck : MqttMessageType {
-    override val code: Byte = 0x0B.toByte()
-}
+    data object Unsubscribe : MqttMessageType {
+        override val code: Byte = 0x0A.toByte()
+    }
 
-data object PingReq : MqttMessageType {
-    override val code: Byte = 0x0C.toByte()
-}
+    data object UnsubAck : MqttMessageType {
+        override val code: Byte = 0x0B.toByte()
+    }
 
-data object PingResp : MqttMessageType {
-    override val code: Byte = 0x0D.toByte()
-}
+    data object PingReq : MqttMessageType {
+        override val code: Byte = 0x0C.toByte()
+    }
 
-data object Disconnect : MqttMessageType {
-    override val code: Byte = 0x0E.toByte()
-}
+    data object PingResp : MqttMessageType {
+        override val code: Byte = 0x0D.toByte()
+    }
 
-data object Auth : MqttMessageType {
-    override val code: Byte = 0x0F.toByte()
+    data object Disconnect : MqttMessageType {
+        override val code: Byte = 0x0E.toByte()
+    }
+
+    data object Auth : MqttMessageType {
+        override val code: Byte = 0x0F.toByte()
+    }
 }
