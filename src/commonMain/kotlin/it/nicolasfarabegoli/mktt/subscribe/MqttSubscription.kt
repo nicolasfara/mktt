bf11f0aed5a0f08754168021b8766b65a4adc444
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-08 15:37:49 +0200

    refactor: remove default argument

--- src/commonMain/kotlin/it/nicolasfarabegoli/mktt/subscribe/MqttSubscription.kt
+++ src/commonMain/kotlin/it/nicolasfarabegoli/mktt/subscribe/MqttSubscription.kt
@@ -1,12 +1,12 @@
 package it.nicolasfarabegoli.mktt.subscribe
 
 import it.nicolasfarabegoli.mktt.message.ExactlyOnce
-import it.nicolasfarabegoli.mktt.message.QoS
+import it.nicolasfarabegoli.mktt.message.MqttQoS
 import it.nicolasfarabegoli.mktt.topic.MqttTopicFilter
 
 data class MqttSubscription(
-    val topicFilter: MqttTopicFilter = MqttTopicFilter(),
-    val qoS: QoS = ExactlyOnce,
+    val topicFilter: MqttTopicFilter,
+    val qoS: MqttQoS = ExactlyOnce,
     val noLocal: Boolean = false,
     val retainHandling: MqttRetainHandling = Send,
     val retainAsPublished: Boolean = false,
