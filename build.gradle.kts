78b639449149198c0a94f6ad35d3f5feb35b6f96
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-08 15:41:45 +0200

    build: configure junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,18 @@ kotlin {
     jvm {
         testRuns["test"].executionTask.configure {
             useJUnitPlatform()
+            filter {
+                isFailOnNoMatchingTests = false
+            }
+            testLogging {
+                showExceptions = true
+                showStandardStreams = true
+                events = setOf(
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+                )
+                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+            }
         }
     }
 
