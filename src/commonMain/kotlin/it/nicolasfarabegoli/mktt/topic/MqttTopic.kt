b7369b114db2cba0448e227ff02e73bd6d326623
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-08 15:38:09 +0200

    refactor: promote at interface

--- src/commonMain/kotlin/it/nicolasfarabegoli/mktt/topic/MqttTopic.kt
+++ src/commonMain/kotlin/it/nicolasfarabegoli/mktt/topic/MqttTopic.kt
@@ -1,15 +1,7 @@
 package it.nicolasfarabegoli.mktt.topic
 
-data class MqttTopic(val value: String) {
-    init {
-        require(value.isNotEmpty()) { "Topic cannot be empty" }
-        require(value.length <= 65535) { "Topic length cannot exceed 65535 characters" }
-    }
-
-    fun levels(): List<String> = value.split(SEPARATOR)
-
-    companion object {
-        const val SEPARATOR = "/"
-        fun String.toMqttTopic(): MqttTopic = MqttTopic(this)
-    }
+interface MqttTopic {
+    val topic: String
+    val levels: List<String>
+    val filter: MqttTopicFilter
 }
