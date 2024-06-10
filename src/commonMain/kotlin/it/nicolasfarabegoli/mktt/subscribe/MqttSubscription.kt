85e4bcd390581ac941f0ae9d6a27197b006c53b6
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-11 00:06:20 +0200

    feat: implement topic and filter and minor refactor to QoS

--- src/commonMain/kotlin/it/nicolasfarabegoli/mktt/subscribe/MqttSubscription.kt
+++ src/commonMain/kotlin/it/nicolasfarabegoli/mktt/subscribe/MqttSubscription.kt
@@ -1,12 +1,11 @@
 package it.nicolasfarabegoli.mktt.subscribe
 
-import it.nicolasfarabegoli.mktt.message.ExactlyOnce
 import it.nicolasfarabegoli.mktt.message.MqttQoS
 import it.nicolasfarabegoli.mktt.topic.MqttTopicFilter
 
 data class MqttSubscription(
     val topicFilter: MqttTopicFilter,
-    val qoS: MqttQoS = ExactlyOnce,
+    val qoS: MqttQoS = MqttQoS.ExactlyOnce,
     val noLocal: Boolean = false,
     val retainHandling: MqttRetainHandling = Send,
     val retainAsPublished: Boolean = false,
