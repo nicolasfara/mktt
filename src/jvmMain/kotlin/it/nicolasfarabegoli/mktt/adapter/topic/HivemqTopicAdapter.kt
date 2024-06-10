5699687a097ee879a03de536901baa4fa7776479
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-11 00:07:05 +0200

    fix: solve problem in adapters

--- src/jvmMain/kotlin/it/nicolasfarabegoli/mktt/adapter/topic/HivemqTopicAdapter.kt
+++ src/jvmMain/kotlin/it/nicolasfarabegoli/mktt/adapter/topic/HivemqTopicAdapter.kt
@@ -2,35 +2,17 @@ package it.nicolasfarabegoli.mktt.adapter.topic
 
 import it.nicolasfarabegoli.mktt.topic.MqttTopic
 import it.nicolasfarabegoli.mktt.topic.MqttTopicFilter
+import it.nicolasfarabegoli.mktt.utils.JavaKotlinUtils.toByteArray
 import com.hivemq.client.mqtt.datatypes.MqttTopic as HiveMqttTopic
-import com.hivemq.client.mqtt.datatypes.MqttTopicFilter as HiveMqttTopicFilter
 
 internal object HivemqTopicAdapter {
     fun HiveMqttTopic.toMqtt(): MqttTopic {
-        val topic = this.toString()
-        val levels = this.levels
-        val filter = this.filter().toMqtt()
-        return object : MqttTopic {
-            override val topic: String = topic
-            override val levels: List<String> = levels
-            override val filter: MqttTopicFilter = filter
-
-        }
+        val topicStringRepr = toByteBuffer().toByteArray().decodeToString()
+        val topic = "topic=([^,]+)".toRegex().find(topicStringRepr)?.groupValues?.get(1)
+            ?: error("Invalid topic string representation")
+        val filterStringRepr = filter().toByteBuffer().toByteArray().decodeToString()
+        val filter = "MqttTopicFilterImpl.+filter=([^,]+)".toRegex().find(filterStringRepr)?.groupValues?.get(1)
+            ?: error("Invalid topic string representation")
+        return MqttTopic.of(topic, MqttTopicFilter.of(filter))
     }
-    fun MqttTopic.toHiveMqtt(): HiveMqttTopic = HiveMqttTopic.of(topic)
-
-    fun HiveMqttTopicFilter.toMqtt(): MqttTopicFilter {
-        val filter = this.toString()
-        val levels = this.levels
-        return object : MqttTopicFilter {
-            override val filter: String = filter
-            override val levels: List<String> = levels
-            override val containsWildcards: Boolean = containsWildcards()
-            override val containsMultilevelWildcard: Boolean = containsMultiLevelWildcard()
-            override val containsSingleLevelWildcard: Boolean = containsSingleLevelWildcard()
-            override fun matches(topic: MqttTopic): Boolean = matches(topic.toHiveMqtt())
-            override fun matches(filter: MqttTopicFilter): Boolean = matches(filter.toHiveMqtt())
-        }
-    }
-    fun MqttTopicFilter.toHiveMqtt(): HiveMqttTopicFilter = HiveMqttTopicFilter.of(filter)
 }
