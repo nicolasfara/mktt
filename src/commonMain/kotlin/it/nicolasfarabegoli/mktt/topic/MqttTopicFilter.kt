247f2531945527af7829a2139eaba176782528fe
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-11 00:06:20 +0200

    feat: implement topic and filter and minor refactor to QoS

--- src/commonMain/kotlin/it/nicolasfarabegoli/mktt/topic/MqttTopicFilter.kt
+++ src/commonMain/kotlin/it/nicolasfarabegoli/mktt/topic/MqttTopicFilter.kt
@@ -8,4 +8,26 @@ interface MqttTopicFilter {
     val containsSingleLevelWildcard: Boolean
     fun matches(topic: MqttTopic): Boolean
     fun matches(filter: MqttTopicFilter): Boolean
+
+    companion object {
+        fun of(filter: String): MqttTopicFilter = MqttTopicFilterImpl(filter)
+        fun String.asTopicFilter(): MqttTopicFilter = MqttTopicFilterImpl(this)
+    }
+
+    private data class MqttTopicFilterImpl(
+        override val filter: String,
+        override val levels: List<String> = filter.split("/"),
+        override val containsWildcards: Boolean = levels.any { it == "#" || it == "+" },
+        override val containsMultilevelWildcard: Boolean = levels.contains("#"),
+        override val containsSingleLevelWildcard: Boolean = levels.contains("+"),
+    ) : MqttTopicFilter {
+        override fun matches(topic: MqttTopic): Boolean {
+            val topicLevels = topic.levels
+            if (topicLevels.size < levels.size) return false
+            return levels.zip(topicLevels).all { (filterLevel, topicLevel) ->
+                filterLevel == "#" || filterLevel == topicLevel || filterLevel == "+"
+            }
+        }
+        override fun matches(filter: MqttTopicFilter): Boolean = filter == this
+    }
 }
