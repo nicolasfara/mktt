122b15a0b7848866cffb47335e8eacb65a3750d1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-12-11 00:49:58 +0100

    build: remove npm publishing

--- release.config.mjs
+++ release.config.mjs
@@ -1,8 +1,8 @@
 const publishCmd = `
 ./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 3
-./gradlew publishJsPackageToNpmjsRegistry || exit 4
 `
-import config from 'semantic-release-preconfigured-conventional-commits' assert { type: "json" };
+import config from 'semantic-release-preconfigured-conventional-commits' assert {type: 'json'};
+
 config.plugins.push(
     [
         "@semantic-release/exec",
