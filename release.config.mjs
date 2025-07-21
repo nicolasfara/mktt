e13e0a46fa1cf684aebc18b931383ebc533a6aa3
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-07-21 09:37:02 +0200

    build: fix release file

--- release.config.mjs
+++ release.config.mjs
@@ -1,16 +1,14 @@
 const publishCmd = `
-git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
-git push --force origin \${nextRelease.version}
-./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 1
-./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication || exit 2
-./gradlew publishAllPublicationsToGithubRepository || true
+./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication || exit 1
 `
-import config from 'semantic-release-preconfigured-conventional-commits' with {type: 'json'};
-
+import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
-    ["@semantic-release/exec", {
-        "publishCmd": publishCmd,
-    }],
+    [
+        "@semantic-release/exec",
+        {
+            "publishCmd": publishCmd,
+        }
+    ],
     "@semantic-release/github",
     "@semantic-release/git",
 )
